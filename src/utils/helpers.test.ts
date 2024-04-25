56f717efd5bd4a89b070ea78ac4570b527ec8959
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-04-25 14:28:44 +0200

    Add basic tests to "test" Bun testing functionality

---
+++ src/utils/helpers.test.ts
@@ -0,0 +1,23 @@
+import { expect, test } from "bun:test";
+import { isValidDate, isValidEmail } from "./helpers";
+
+test("isValidDate", () => {
+  expect(isValidDate("2024-02-29")).toBe(true); // 2024 is a leap year
+  expect(isValidDate("2023-02-29")).toBe(false); // 2023 is not a leap year
+  expect(isValidDate("2021-01-01")).toBe(true);
+  expect(isValidDate("2021-01-32")).toBe(false);
+  expect(isValidDate("2021-13-01")).toBe(false);
+  expect(isValidDate("2021-12-31")).toBe(true);
+  expect(isValidDate("hello")).toBe(false);
+  expect(isValidDate({})).toBe(false);
+});
+
+test("isValidEmail", () => {
+  expect(isValidEmail("example@mail.foo")).toBe(true);
+  expect(isValidEmail("example@mail")).toBe(false);
+  expect(isValidEmail("example.mail.foo")).toBe(false);
+  expect(isValidEmail("example@mail.")).toBe(false);
+  expect(isValidEmail(123)).toBe(false);
+  expect(isValidEmail({})).toBe(false);
+  expect(isValidEmail("he@ll.ou")).toBe(true);
+});

d817c0665d7cb4bc15908dc9115a6a4bf4c07667
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-05-02 23:19:53 +0200

    Add type checking to formatters.ts / Install lodash

---
+++ src/types/Person.ts
@@ -0,0 +1,11 @@
+export interface Person {
+  id: number;
+  forename: string;
+  surname: string;
+  second_surname: string;
+  email: string;
+  phone_code: string;
+  phone_number: string;
+  birth: string;
+  dni: string;
+}
--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -1,25 +1,35 @@
-function formatBuilding(building) {
-  delete building.president_dni;
-  const president = building.people_buildings_president_dniTopeople;
-  delete building.people_buildings_president_dniTopeople;
+import { buildings } from '@prisma/client';
+import { Person } from '../types/Person';
+import _ from 'lodash';
+
+// 'buildings' is the type of the object returned by Prisma
+interface Building extends buildings {
+  people_buildings_president_dniTopeople?: Person;
+}
+
+function formatBuilding(building: Building) {
+  // Change the name of the key for better readability
+  const buildingClone = _.cloneDeep(building);
+  const president = buildingClone.people_buildings_president_dniTopeople;
+  delete buildingClone.people_buildings_president_dniTopeople;
+
+  // I would also remove the 'presindent_dni' key, but I get an error
+  // delete buildingClone.president_dni;
 
   return {
     president: president,
-    ...building,
-  }
+    ...buildingClone,
+  };
 }
 
-function formatBuildingList(buildings) {
+function formatBuildingList(buildings: Array<Building>) {
   return buildings.map((building) => {
     return {
       id: building.id,
       title: `${building.address_type} ${building.street_address} ${building.number}`,
       district: building.district,
-    }
-  })
+    };
+  });
 }
 
-export {
-  formatBuilding,
-  formatBuildingList,
-}
+export { formatBuilding, formatBuildingList };

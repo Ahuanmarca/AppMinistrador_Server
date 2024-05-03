4f61a5fed28346b5b928eb452c58170d8701f808
Renzo Belon <75696141+Ahuanmarca@users.noreply.github.com>
2024-05-03 11:45:33 +0200

    Add type annotations on incidences route
    - Created IncidenceInput type (on types directory)

--- src/service/incidences.service.ts
+++ src/service/incidences.service.ts
@@ -1,15 +1,16 @@
 import * as incidencesRepository from '../repository/incidences.repository';
+import { IncidenceInput } from '../types/IncidenceInput';
 
 async function getAllIncidences() {
   return await incidencesRepository.getAllIncidences();
 }
 
-async function createIncidence(incidence) {
+async function createIncidence(incidence: IncidenceInput) {
   const newIncidence = await incidencesRepository.createIncidence(incidence);
   return newIncidence;
 }
 
-async function updateStatus(id, status) {
+async function updateStatus(id: number | string, status: string) {
   const updatedIncidence = await incidencesRepository.updateStatus(id, status);
   return updatedIncidence;
 }
---
+++ src/types/IncidenceInput.ts
@@ -0,0 +1,8 @@
+export interface IncidenceInput {
+  title: string;
+  description: string;
+  image_url: string;
+  user_dni: string;
+  building_id: number;
+  category: string;
+}
